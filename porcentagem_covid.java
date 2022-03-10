6659fe92a360491ed9c03d46f1a20709fa7d720d
Geovana Borba <98980485+geovanaborba@users.noreply.github.com>
2022-03-10 18:49:12 -0300

    Update porcentagem_covid.java

--- porcentagem_covid.java
+++ porcentagem_covid.java
@@ -1,3 +1,15 @@
+/*Escreva um fluxograma e a codificação em Java para calcular o percentual de casos de coronavírus nas regiões do Brasil. Para tanto, o programa deve ler os seguintes dados:
+1. Número de casos no Brasil
+2. Nome da região
+3. Número de casos da região
+ 
+O programa deve exibir na tela um painel de dados com as seguintes informações:
+1. Nome da região
+2. Número de casos no Brasil
+3. Número de casos digitado
+4. Porcentual que a região representa em relação ao total de casos no Brasil*/
+
+
 import java.util.Scanner;
 
 public class porcentagem_covid {
