e80e8b25878956196b51916bcab5cf56d2ea443f
Geovana Borba <98980485+geovanaborba@users.noreply.github.com>
2023-01-12 17:06:26 -0300

    Create index.php

---
+++ PHP/Agenda 6 - Exibição de tabela utilizando banco de dados SQL/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<title>Escola de Inglês</title>
+</head>
+<body>
+<div class="w3-padding w3-text-grey w3-half w3-display-middle w3-center">
+    <h1 class="w3-center w3-pink w3-round-large w3-margin">Notas dos Alunos</h1>
+        <div class="w3-col w3-button w3-blue w3-cell w3-round-large w3-right w3-center" style="width:100%;">
+            <a href="return.php" style="text-decoration: none;"> 
+                <i class="fa fa-vcard-o" style="font-size: 10.5em"></i> 
+                <p style="font-size: 2em">Listar Notas</p>
+            </a>
+        <div>
+    </div>
+</div>
+</body>
+</html>
